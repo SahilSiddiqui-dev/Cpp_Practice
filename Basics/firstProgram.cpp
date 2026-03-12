113c0823317efa97e957bc3cf3a780ee1e9a3b76
Mohd Sahil <sahilsiddiqui2424@gmail.com>
2026-03-12 22:48:22 +0530

    adding new files to the repository

---
+++ Basics/firstProgram.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout <<"Hellow World!"<< endl<< "This is my first C++ program.";
+    cout<< endl; // Adding a newline for better output formatting
+    cout <<"Hellow World!";
+    return 0;
+}
