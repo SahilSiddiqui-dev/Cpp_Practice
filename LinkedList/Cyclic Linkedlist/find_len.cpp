9efae09e5d6cb8febed219ac4f0132740ed02d36
SahilSiddiqui-dev <sahil.siddiqui.dev0@gmail.com>
2026-08-02 23:41:12 +0530

    add: implement cycle detection and length finding for cyclic linked list

--- LinkedList/Cyclic Linkedlist/find_len.cpp
+++ LinkedList/Cyclic Linkedlist/find_len.cpp
@@ -24,3 +24,52 @@ Node* creation(int arr[], int n){
     
    return head;
 }
+bool detect_cycle(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    while(slow != NULL && fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            return 1;
+        }
+    }
+    return 0;
+}
+int findLen(Node* head){
+    Node* slow = head;
+    Node* fast = head;
+    int cnt = 0;
+    while(slow != NULL && fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        Node* temp = NULL;
+        
+
+        if(slow == fast){
+            temp = slow;
+            slow = slow->next;
+            while(slow != temp){
+                cnt++;
+                slow = slow->next;
+            }
+            return cnt;
+        }
+    }
+    return 0;
+}
+int main() {
+int arr[] = {1, 2, 3, 4, 5};
+Node* head = creation(arr, 4);
+
+if(detect_cycle(head)){
+    cout << "Cyclic Linked List" << endl;
+}
+else{
+    cout << "Not a Cyclic Linked List" << endl;
+}
+cout << endl;
+int x =  findLen(head);
+cout << x;
+return 0;
+}
