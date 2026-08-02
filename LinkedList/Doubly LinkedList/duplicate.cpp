6d59182023ad56d8de5d913c29563c8c26492626
SahilSiddiqui-dev <sahil.siddiqui.dev0@gmail.com>
2026-08-02 21:07:30 +0530

    refactor: improve duplicate removal logic in doubly linked list implementation

--- LinkedList/Doubly LinkedList/duplicate.cpp
+++ LinkedList/Doubly LinkedList/duplicate.cpp
@@ -22,26 +22,37 @@ Node* creation(int arr[], int index, int size, Node* prev){
     return temp;
 }
 Node* duplicate(Node* head){
-    Node* prev = head;
-    Node* curr = prev->next;
-    while(curr){
-        Node* front = curr->next;
-        if(prev->data == curr->data){
-            prev->next = front;
-            front->prev = prev;
-            prev = curr;
-            curr = front;
+    Node* slow = head;
+    while(slow){
+        Node* fast = slow->next;
+        while(fast){
+            if(slow->data == fast->data){
+                if(fast->next == NULL){
+                    Node* last = fast->prev;
+                    delete fast;
+                    last->next = NULL;
+                    fast = NULL;
+                }
+                else {
+                    Node* last = fast->prev;
+                    Node* curr = fast;
+                    fast = fast->next;
+                    last->next = fast;
+                    fast->prev = last;
+                    delete curr;
+                }
+            }
+            else {
+                fast = fast->next;
+            }
         }
-        else {
-            prev = curr;
-            curr = curr->next;
-        }
+            slow = slow->next;
     }
     return head;
 }
 int main(){
-    int arr[] = {1, 2, 2, 2, 2};
-    Node* head = creation(arr, 0, 5, NULL);
+    int arr[] = {1, 2, 2, 2,2,3, 4, 7, 7, 8, 9, 9};
+    Node* head = creation(arr, 0, 12, NULL);
     Node* temp = head;
     while(temp){
         cout << temp->data << " ";
