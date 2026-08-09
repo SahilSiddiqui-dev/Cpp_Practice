b05949c8ddd0b0a3d3350cb4c576bc892f5bbac2
Mohd Sahil <sahil.siddiqui.dev0@gmail.com>
2026-08-10 00:25:18 +0530

    Time: 7 ms (51.63%) | Memory: 14.9 MB (96.49%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -16,14 +16,16 @@ public:
 
 class Solution {
 public:
-    unordered_map<Node*, Node*>find;
     Node* copyRandomList(Node* head) {
         Node* Copyhead = new Node(0);
         Node* tail = Copyhead;
         Node* temp = head;
+        Node* front1;
+        Node* front2;
+        Node* curr1;
+        Node* curr2;
         while(temp){
             tail->next = new Node(temp->val);
-            find[temp] = tail->next;
             temp = temp->next;
             tail = tail->next;
         }
@@ -31,17 +33,37 @@ public:
         Node* toDelete = Copyhead;
         Copyhead = Copyhead->next;
         delete toDelete;
-        tail = Copyhead;
+        curr1 = head;
+        curr2 = Copyhead;
+        while(curr1){
+            front1 = curr1->next;
+            front2 = curr2->next;
+            curr1->next = curr2;
+            curr2->next = front1;
+            curr1 = front1;
+            curr2 = front2;
+        }
+
         temp = head;
 
-        while(temp){
-            Node* search = temp->random;
-            if(search == NULL){
-                tail->random = NULL;
+        while(temp){ 
+            tail = temp->next;
+            Node* find = temp->random;
+            if(find){
+                tail->random = find->next;
             }
-            tail->random = find[search];
-            temp = temp->next;
-            tail = tail->next;
+            temp = tail->next;
+        }
+
+        curr1 = head;
+        curr2 = Copyhead;
+        while(curr1){
+        curr1->next = curr1->next->next;
+        curr2->next = curr2->next ? curr2->next->next : nullptr;
+
+        curr1 = curr1->next;
+        curr2 = curr2->next;
+        
         }
         return Copyhead;
     }
