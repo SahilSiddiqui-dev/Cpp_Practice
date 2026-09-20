9ce24cc4ee1dec3797228c757b7bc3578f98d5a2
Mohd Sahil <sahil.siddiqui.dev0@gmail.com>
2026-09-20 17:22:40 +0530

    Time: 2 ms (19.75%) | Memory: 10 MB (8.02%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        vector<char>alpha;
+        for(char c = 'z'; c >= 'a'; c--){
+            alpha.push_back(c);
+        }
+        int sum = 0;
+        for(int i = 0; i < s.size(); i++){
+            auto it = find(alpha.begin(), alpha.end(), s[i]);
+            if(it != alpha.end()){
+                int index = distance(alpha.begin(), it);
+                sum += ((index + 1) * (i + 1));
+            }
+        }
+        return sum;
+    }
+};
